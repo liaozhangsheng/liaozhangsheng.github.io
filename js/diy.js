df5e0837c2e8b3fdbc5ab06affef485c67c5bf68
liaozhangsheng <liaozhangsheng@163.com>
2022-06-29 21:34:38 +0800

    Site updated: 2022-06-29 21:34:37

--- js/diy.js
+++ js/diy.js
@@ -1,9 +1,9 @@
 /*随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号*/
 var backimg =[
-    "url(https://raw.githubusercontent.com/liaozhangsheng/picbed/main/rbg1.jpg)",
-    "url(https://raw.githubusercontent.com/liaozhangsheng/picbed/main/rbg3.jpg)",
-    "url(https://raw.githubusercontent.com/liaozhangsheng/picbed/main/rbg2.jpg)",
-    "url(http://rdpxkthcp.hn-bkt.clouddn.com/bg.jpg)",
+    "url(/img/rbg1.jpg)",
+    "url(/img/rbg3.jpg)",
+    "url(/img/rbg2.jpg)",
+    "url(/img/bg.jpg)",
     ];
     /* //获取背景图片总数，生成随机数 */
     var bgindex =Math.ceil(Math.random() * (backimg.length-1));
